822eed789b12477090ff5ae8259e6d2b42869d49
jowashli <jl.pytiger@gmail.com>
2026-06-17 08:08:03 +0800

    Create block title creation parameter

--- scripts/BlockCreationMenu.gd
+++ scripts/BlockCreationMenu.gd
@@ -3,13 +3,20 @@ extends VBoxContainer
 @export var SaveButton : Button
 @export var CancelButton : Button
 
+@export var TitleInput : LineEdit
+
 func _ready () -> void:
 	SaveButton.pressed.connect(save)
 	CancelButton.pressed.connect(cancel)
 
 func save () -> void:
-	BlockManager.create_and_add_block("new")
+	BlockManager.create_and_add_block(TitleInput.text)
+	reset()
 	MenuManager.set_menu(0)
 
 func cancel () -> void:
+	reset()
 	MenuManager.set_menu(0)
+
+func reset () -> void:
+	TitleInput.text = ""
